4e5b34ed13926afe73ba8ffaa486158d34e9b4e5
Gorobets Nikita <sawwa.cwer13@gmail.com>
2024-10-25 12:54:42 +0300

    Added EditingVideoService and his interface and rename some files

--- Media/Program.cs
+++ Media/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Media.src;
+using Media.src.objects;
 using MEDIA.src;
 using MEDIA.src.objects;
 
@@ -11,11 +13,17 @@ namespace MEDIA
     {
         public static async Task Main()
         {
-            List<string> videos = new List<string>() { "https://www.youtube.com/shorts/ZPiYFU4pqiQ" };
-            string downloadPath = "/home/mcniki/Видео/";
+            // List<string> videos = new List<string>() { "https://www.youtube.com/shorts/ZPiYFU4pqiQ" };
+            // string downloadPath = "/home/mcniki/Видео/";
 
-            IDownloadVideoService downloadVideo = new DownloadVideosService(videos, downloadPath);
-            await downloadVideo.DownloadVideos();
+            // IDownloadVideoService downloadVideo = new DownloadVideosService(videos, downloadPath);
+            // await downloadVideo.DownloadVideos();
+
+            List<string> videosPath = new List<string>() {"/home/mcniki/Видео/video.webm"};
+            string timePartVideo = "00:00:30";
+
+            IEditingVideoService editingVideo = new EditingVideoService(videosPath, timePartVideo);
+            await editingVideo.CutVideos();
         }
     }
 }
